ae58359ece487029d9c2e8b4f93bea0b8fda4b2f
Travis Herrick <tthetoad@gmail.com>
2015-05-23 21:04:41 -0700

    Raise an error if app is initialized multiple times

--- lib/base_jump/application.rb
+++ lib/base_jump/application.rb
@@ -2,7 +2,8 @@ module BaseJump
   module Application
     extend self
 
-    def set(app)
+    def init(app)
+      raise ApplicationInitializedError.new(@app) if defined?(@app)
       @app = app
     end
 
--- spec/lib/base_jump/application_spec.rb
+++ spec/lib/base_jump/application_spec.rb
@@ -1,13 +1,22 @@
 require 'spec_helper'
 
 RSpec.describe BaseJump::Application do
-  describe '.set' do
-    let(:namespace) { 'application-namespace' }
+  describe '.init' do
+    let(:app_namespace) { 'application-namespace' }
 
-    before { described_class.set namespace }
+    before { described_class.instance_variable_set :@app, nil }
+    before { described_class.remove_instance_variable :@app }
+    before { described_class.init app_namespace }
 
     it 'sets the application namespace' do
-      expect(described_class.app).to eq namespace
+      expect(described_class.app).to eq app_namespace
+    end
+
+    context 'given the application has been set' do
+      it 'raises an error' do
+        expect{described_class.init(app_namespace)}
+          .to raise_error BaseJump::ApplicationInitializedError
+      end
     end
   end
 end
