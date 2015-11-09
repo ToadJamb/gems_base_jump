0e5dd5141702ea774095ab232d469181f111e2c7
Travis Herrick <tthetoad@gmail.com>
2015-11-08 20:52:51 -0800

    Log with elapsed time

--- lib/base_jump/application.rb
+++ lib/base_jump/application.rb
@@ -33,6 +33,23 @@ module BaseJump
       conf
     end
 
+    def log_with_time(description, level = :info, &block)
+      return unless block_given?
+
+      color   = nil
+      started = nil
+
+      logger.send(level) do
+        color       = next_color_for(description)
+        description = ColorHelper.colorize(description, color)
+        started     = Time.now
+
+        "#{starting} #{description}"
+      end
+
+      finish_block_for description, level, started, &block
+    end
+
     def run_quietly(&block)
       return unless block_given?
 
@@ -54,6 +71,56 @@ module BaseJump
 
     def configuration
       Backpack.configuration
+    end
+
+    def next_color_for(description)
+      ColorHelper.next_color unless ColorHelper.colored?(description)
+    end
+
+    def starting
+      @starting ||= ColorHelper.colorize('Starting', :green)
+    end
+
+    def finished
+      @finished ||= ColorHelper.colorize('Finished', :red)
+    end
+
+    def finish_block_for(description, level, started, &block)
+      yield
+
+      logger.send(level) do
+        "#{finished} #{description} in #{elapsed_from(started)}"
+      end
+    end
+
+    def elapsed_from(started)
+      seconds = Time.now - started
+      hash = interval_for(seconds)
+      format_elapsed hash[:interval], hash[:descriptor]
+    end
+
+    def interval_for(seconds)
+      hash = {}
+
+      if seconds >= 60 * 60
+        hash[:interval] = seconds / (60 * 60.0)
+        hash[:descriptor] = 'hour'
+      elsif seconds >= 60
+        hash[:interval] = seconds / 60.0
+        hash[:descriptor] = 'minute'
+      else
+        hash[:interval] = seconds
+        hash[:descriptor] = 'second'
+      end
+
+      hash
+    end
+
+    def format_elapsed(interval, descriptor)
+      descriptor = descriptor.pluralize(interval)
+      interval   = '%.2f' % interval
+
+      ColorHelper.colorize "#{interval} #{descriptor}", :light_magenta
     end
   end
 end
