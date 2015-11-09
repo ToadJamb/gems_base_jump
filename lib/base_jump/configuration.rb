7b0445d6a9dec89c22cf421ed2dd7acd1d0c9e51
Travis Herrick <tthetoad@gmail.com>
2015-11-08 16:17:23 -0800

    custom formatter

--- lib/base_jump/configuration.rb
+++ lib/base_jump/configuration.rb
@@ -3,17 +3,21 @@ module BaseJump
     attr_writer :log_destination
     attr_writer :log_path
     attr_writer :log_level
+    attr_writer :custom_formatter
 
     attr_accessor :initializers
+    attr_accessor :color_log
 
     def initialize
       @log_file = "#{Backpack.app.environment}.log"
       @log_path = 'log'
 
+      @color_log = false
+      @color_log = true if Backpack.app.environment == :development
+
+      @log_level = Logger::INFO
       if [:test, :development].include?(Backpack.app.environment)
         @log_level = Logger::DEBUG
-      else
-        @log_level = Logger::INFO
       end
 
       @initializers = []
@@ -37,8 +41,13 @@ module BaseJump
       @logger
     end
 
+    def custom_formatter
+      return @custom_formatter if defined?(@custom_formatter)
+    end
+
     def logger=(new_logger)
       new_logger.level = @log_level
+      new_logger.custom_formatter = custom_formatter
       @logger = new_logger
     end
   end
